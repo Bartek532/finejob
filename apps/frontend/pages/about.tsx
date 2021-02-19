dd87d67a48e48c932abee7ccf86d0852c752eeed
Bartek532 <57185551+Bartek532@users.noreply.github.com>
2021-02-19 21:21:25 +0100

    add first mockups, descriptions and styles for about section

--- apps/frontend/pages/about.tsx
+++ apps/frontend/pages/about.tsx
@@ -1,10 +1,12 @@
 import { Layout } from "../components/Layout/Layout";
 import { AboutSection } from "../views/AboutSection/AboutSection";
+import { Footer } from "../components/Footer/Footer";
 
 const About = () => {
   return (
     <Layout>
       <AboutSection />
+      <Footer />
     </Layout>
   );
 };
--- apps/frontend/views/AboutSection/AboutSection.tsx
+++ apps/frontend/views/AboutSection/AboutSection.tsx
@@ -1,17 +1,18 @@
 import styles from "./AboutSection.module.scss";
+import Image from "next/image";
 
 const steps = [
   {
     title: "Search",
     image: "search",
     description:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti, optio velit voluptatum nihil necessitatibus obcaecati aliquam tempore ipsam, deserunt a numquam qui porro dolores fugiat soluta libero eius exercitationem quis.",
+      "Finejob provides you a couple of ways to search. You can search <b>by keyword</b>, <b>by location</b> and many more. Imagine, you can find a job that is near you! We search thousands of offers, use the latest technologies to find what you are looking for!",
   },
   {
     title: "Choose",
     image: "choose",
     description:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti, optio velit voluptatum nihil necessitatibus obcaecati aliquam tempore ipsam, deserunt a numquam qui porro dolores fugiat soluta libero eius exercitationem quis.",
+      "After you tell us what you are looking for we listing you dozens of offers. But don't worry! First, you will see only first <b>50</b> offers, so you can carefully and efficiently choose. But if you can't find anything for yourself, load more offers! We will inform you when the offers end and you have to search again. <b>Don't give up!</b>",
   },
   {
     title: "Save",
@@ -39,12 +40,22 @@ export const AboutSection = () => {
       <h1 className={styles.title}>how it works</h1>
       <div className={styles.steps}>
         {steps.map(step => (
-          <article className={styles.step}>
+          <article className={styles.step} key={step.title}>
             <div className={styles.description}>
               <h3>{step.title}</h3>
-              <p className={styles.text}>{step.description}</p>
+              <p
+                className={styles.text}
+                dangerouslySetInnerHTML={{ __html: step.description }}
+              ></p>
             </div>
-            <div className={styles.image}></div>
+            <div className={styles.image}>
+              <Image
+                src={`/images/mockups/${step.image}.png`}
+                alt={step.image}
+                width={320}
+                height={220}
+              />
+            </div>
           </article>
         ))}
       </div>
