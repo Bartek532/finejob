311058fb254549379cee905a14252dd2ed9722c5
Bartek532 <57185551+Bartek532@users.noreply.github.com>
2021-02-25 16:54:21 +0100

    add lerna

--- packages/api/src/utils.ts
+++
@@ -1,5 +0,0 @@
-import type { Offer } from "./types";
-
-export const addRandomSalaryToOffer = (item: Offer) => {
-  return { ...item, salary: Math.floor(Math.random() * (30000 - 4000)) + 4000 };
-};
