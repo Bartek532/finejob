f2496b3c59fdb6c3a7dd7be7328405318a4ebe08
Bartek532 <57185551+Bartek532@users.noreply.github.com>
2021-03-06 16:59:46 +0100

    add theme switcher

--- apps/frontend/components/ThemeSwitch/ThemeSwitch.tsx
+++ apps/frontend/components/ThemeSwitch/ThemeSwitch.tsx
@@ -1,14 +1,28 @@
 import styles from "./ThemeSwitch.module.scss";
-import { memo } from "react";
+import { memo, useEffect } from "react";
+import { useLocalStorage } from "../../lib/hooks/useLocalStorage";
 import Image from "next/image";
 
+type ThemePreferences = "dark" | "light";
+
 export const ThemeSwitch = memo(() => {
+  const [theme, setTheme] = useLocalStorage<ThemePreferences>("theme", "light");
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme || "light");
+  }, [theme]);
+
   return (
     <label className={styles.switch}>
-      <input type="checkbox" className={styles.input} />
+      <input
+        type="checkbox"
+        className={styles.input}
+        checked={theme === "dark"}
+        onChange={() => setTheme(theme === "dark" ? "light" : "dark")}
+      />
       <div className={styles.slider}>
         <div className={styles.circle}>
-          <Image src="/icons/theme/light.svg" width={14} height={14} />
+          <Image src={`/icons/theme/${theme}.svg`} width={14} height={14} />
         </div>
       </div>
     </label>
