54e1f9983455722d2a80d15a2fb3e3e93624fbff
Bartek532 <57185551+Bartek532@users.noreply.github.com>
2021-07-22 21:42:54 +0200

    add some helper funcitons

--- apps/api/src/utils.ts
+++ apps/api/src/utils.ts
@@ -1,3 +1,12 @@
+import type { Offer } from "@finejob/types";
+
 export const getRandomSalary = () => {
   return Math.floor(Math.random() * (30000 - 4000)) + 4000;
+};
+
+export const normalizeSalaryInOffer = (offer: Offer) => {
+  return {
+    ...offer,
+    salary: offer.employment_types[0]?.salary?.to || getRandomSalary(),
+  };
 };
