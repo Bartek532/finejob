9927b3dddf0744507f6b124becf478d33de6592a
Bartek532 <57185551+Bartek532@users.noreply.github.com>
2021-07-22 21:27:45 +0200

    delete utils file

--- apps/api/src/utils.ts
+++
@@ -1,5 +0,0 @@
-import type { Offer, OfferWithSalary } from "@finejob/types";
-
-export const addRandomSalaryToOffer = (item: Offer): OfferWithSalary => {
-  return { ...item, salary: Math.floor(Math.random() * (30000 - 4000)) + 4000 };
-};
